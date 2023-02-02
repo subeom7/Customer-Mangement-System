bb513ccc1fae7e4b42400bbea98d6bdfffdd0108
Subeom7 <subeomkwon@gmail.com>
2023-02-01 22:25:38 -0500

    implemented Material-UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import Customer from './components/Customer';
-import { Component } from 'react';
+import React, { Component }  from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell'; 
+import { withStyles } from '@mui/styles';
 
+
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    // marginTop: theme.spacing(3),
+    overFlowX: "auto"
+  },
+  table:{
+    minWidth: 1080
+  }
+})
 const customers = [{
   'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
+  'image': 'https://xsgames.co/randomusers/avatar.php?g=pixel',
   'name': 'customer1',
   'birthday': '1/1/2002',
   'gender': 'male',
@@ -12,7 +31,7 @@ const customers = [{
 },
 {
   'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
+  'image': 'https://xsgames.co/randomusers/assets/avatars/pixel/24.jpg',
   'name': 'customer2',
   'birthday': '4/3/1999',
   'gender': 'male',
@@ -20,7 +39,7 @@ const customers = [{
 },
 {
   'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
+  'image': 'https://xsgames.co/randomusers/assets/avatars/pixel/33.jpg',
   'name': 'customer3',
   'birthday': '5/3/2003',
   'gender': 'Female',
@@ -29,27 +48,28 @@ const customers = [{
 
 class App extends Component {
   render(){
+    const {classes} = this.props; 
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-              <Customer 
-              key={c.id}
-              id ={c.id}
-              image = {c.image}
-              name = {c.name}
-              birthday = {c.birthday}
-              gender = {c.gender}
-              job = {c.job}
-              />
-            ) 
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+        <Table classname={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Image</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Birthday</TableCell>
+              <TableCell>Gender</TableCell>
+              <TableCell>Job</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+        {customers.map(c => {return (<Customer key={c.id} id ={c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job}/>)})}  
+        </TableBody>     
+        </Table>
+      </Paper>
     );
   }
 
 }
 
-export default App;
+export default withStyles(styles)(App);
